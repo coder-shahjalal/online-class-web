809d7b907fa0bb5e1ddf299bc850a8ed055b1397
coder-shahjalal <shahjalal.freelancer@gmail.com>
2020-08-28 00:56:37 +0600

    bootstrap add successfull

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <button className=" btn btn-success">Bootstrap</button>
+      <button className="btn btn-success">Bootstrap</button>
     </div>
   );
 }
